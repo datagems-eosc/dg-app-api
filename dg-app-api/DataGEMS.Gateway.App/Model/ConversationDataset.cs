a9323d9522b6db7dfc9db9d7c4c77ac661639c70
Isidoros Avdeliodis <iavdeliodis@cite.gr>
2025-06-27 13:38:31 +0300

    Data creation (Conversation, ConversationDataset, ConversationMessage) and start of model creation

---
+++ dg-app-api/DataGEMS.Gateway.App/Model/ConversationDataset.cs
@@ -0,0 +1,20 @@
+﻿using DataGEMS.Gateway.App.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGEMS.Gateway.App.Model
+{
+	public class ConversationDataset
+	{
+		public Guid? Id { get; set; }
+		public Dataset Dataset { get; set; }
+		public Conversation Conversation { get; set; }
+		public IsActive? IsActive { get; set; }
+		public DateTime? CreatedAt { get; set; }
+		public DateTime? UpdatedAt { get; set; }
+		public String ETag { get; set; }
+	}
+}
