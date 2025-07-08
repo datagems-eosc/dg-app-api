bca244474352d37ba362a4630cf3f698c97829ac
Isidoros Avdeliodis <iavdeliodis@cite.gr>
2025-07-08 16:02:24 +0300

    #1: Controller endpoint Censor [WIP]

--- dg-app-api/DataGEMS.Gateway.Api/Controllers/SearchController.cs
+++ dg-app-api/DataGEMS.Gateway.Api/Controllers/SearchController.cs
@@ -10,6 +10,12 @@ using Cite.Tools.Logging;
 using Cite.Tools.Data.Censor;
 using Cite.Tools.Logging.Extensions;
 using DataGEMS.Gateway.App.Accounting;
+using Cite.Tools.FieldSet;
+using DataGEMS.Gateway.App.Censor;
+using DataGEMS.Gateway.App.Common;
+using DataGEMS.Gateway.App.Data;
+using DataGEMS.Gateway.App.Exception;
+using DataGEMS.Gateway.App.ErrorCode;
 
 namespace DataGEMS.Gateway.Api.Controllers
 {
@@ -21,15 +27,20 @@ namespace DataGEMS.Gateway.Api.Controllers
 		private readonly ICrossDatasetDiscoveryService _crossDatasetDiscoveryService;
 		private readonly ILogger<SearchController> _logger;
 		private readonly IAccountingService _accountingService;
+		private readonly ErrorThesaurus _errors;
 
 		public SearchController(
+			CensorFactory censorFactory,
 			ICrossDatasetDiscoveryService crossDatasetDiscoveryService,
 			IAccountingService accountingService,
-			ILogger<SearchController> logger)
+			ILogger<SearchController> logger,
+			ErrorThesaurus errors)
 		{
+			this._censorFactory = censorFactory;
 			this._crossDatasetDiscoveryService = crossDatasetDiscoveryService;
 			this._accountingService = accountingService;
 			this._logger = logger;
+			this._errors = errors;
 		}
 
 		[HttpPost("cross-dataset")]
@@ -52,13 +63,16 @@ namespace DataGEMS.Gateway.Api.Controllers
 		{
 			this._logger.Debug(new MapLogEntry("cross dataset discovering").And("type", nameof(App.Model.CrossDatasetDiscovery)).And("lookup", lookup));
 
+			IFieldSet censoredFields = await this._censorFactory.Censor<CrossDatasetDiscoveryCensor>().Censor(lookup.Project, CensorContext.AsCensor());
+			if (lookup.Project.CensoredAsUnauthorized(censoredFields)) throw new DGForbiddenException(this._errors.Forbidden.Code, this._errors.Forbidden.Message);
+
 			DiscoverInfo request = new DiscoverInfo()
 			{
 				Query = lookup.Query,
 				ResultCount = lookup.ResultCount
 			};
 
-			List<CrossDatasetDiscovery> results = await this._crossDatasetDiscoveryService.DiscoverAsync(request, lookup.Project);
+			List<CrossDatasetDiscovery> results = await this._crossDatasetDiscoveryService.DiscoverAsync(request, censoredFields);
 
 			this._accountingService.AccountFor(KnownActions.Invoke, KnownResources.CrossDatasetDiscovery.AsAccountable());
 
