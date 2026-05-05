8bf744f337a4b5fd9945ff83a2079d07e16339f5
voskakiscite <kvoskakis@cite.gr>
2026-05-05 10:35:16 +0300

    #62 create db migration

---
+++ db/01.07.000-ad_hoc_query_result.AddTable.sql
@@ -0,0 +1,22 @@
+CREATE TABLE if not exists public.ad_hoc_query_result (
+    id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    analytical_pattern TEXT NOT NULL,
+    is_active SMALLINT NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+    CONSTRAINT as_hoc_query_result_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_ad_hoc_query_result_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+UPDATE version_info 
+SET 
+  version = '01.06.000',
+  released_at = '2026-05-05 00:00:00.00000+00', 
+  deployed_at = now(),
+  description = 'CreateTable.AdHocQueryResult'
+WHERE key = 'DataGEMS.Gateway.db'
