6b656123bda3167c2b82997829fb18e49820234c
voskakiscite <kvoskakis@cite.gr>
2026-05-20 11:35:21 +0300

    #70 controller and viewmodel scaffolding

---
+++ src/DataGEMS.Gateway.Api/Controllers/PilotController.cs
@@ -0,0 +1,50 @@
+﻿using Cite.Tools.Data.Builder;
+using Cite.Tools.Data.Censor;
+using Cite.Tools.Data.Query;
+using Cite.WebTools.Validation;
+using DataGEMS.Gateway.Api.Validation;
+using DataGEMS.Gateway.App.Accounting;
+using DataGEMS.Gateway.App.ErrorCode;
+using DataGEMS.Gateway.App.Service.DataManagement;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace DataGEMS.Gateway.Api.Controllers
+{
+	[Route("api/pilot")]
+	[ApiController]
+	public class PilotController : ControllerBase
+	{
+		private readonly CensorFactory _censorFactory;
+		private readonly QueryFactory _queryFactory;
+		private readonly BuilderFactory _builderFactory;
+		private readonly ILogger<PilotController> _logger;
+		private readonly IAccountingService _accountingService;
+		private readonly IDataManagementService _datasetService;
+		private readonly ErrorThesaurus _errors;
+		private readonly IStringLocalizer<DataGEMS.Gateway.Resources.MySharedResources> _localizer;
+
+		public PilotController(
+			CensorFactory censorFactory,
+			QueryFactory queryFactory,
+			BuilderFactory builderFactory,
+			ILogger<PilotController> logger,
+			IAccountingService accountingService,
+			IDataManagementService datasetService,
+			ErrorThesaurus errors,
+			IStringLocalizer<DataGEMS.Gateway.Resources.MySharedResources> localizer)
+		{
+			this._censorFactory = censorFactory;
+			this._queryFactory = queryFactory;
+			this._builderFactory = builderFactory;
+			this._logger = logger;
+			this._accountingService = accountingService;
+			this._datasetService = datasetService;
+			this._errors = errors;
+			this._localizer = localizer;
+		}
+
+	}
+}
---
+++ src/DataGEMS.Gateway.App/Model/LanguagePilot.cs
@@ -0,0 +1,89 @@
+﻿using Cite.Tools.Validation;
+using DataGEMS.Gateway.App.Common.Validation;
+using DataGEMS.Gateway.App.ErrorCode;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace DataGEMS.Gateway.App.Model
+{
+	public class LanguagePilotRequest
+	{
+		public string Query { get; set; }
+		public List<Guid> DatasetIds { get; set; }
+
+		public class RequestValidator : BaseValidator<LanguagePilotRequest>
+		{
+			public RequestValidator(
+				IStringLocalizer<DataGEMS.Gateway.Resources.MySharedResources> localizer,
+				ValidatorFactory validatorFactory,
+				ILogger<RequestValidator> logger,
+				ErrorThesaurus errors) : base(validatorFactory, logger, errors)
+			{
+				this._localizer = localizer;
+			}
+
+			private readonly IStringLocalizer<DataGEMS.Gateway.Resources.MySharedResources> _localizer;
+
+			protected override IEnumerable<ISpecification> Specifications(LanguagePilotRequest item)
+			{
+				return [
+					//query must always be set
+					this.Spec()
+						.Must(() => !this.IsEmpty(item.Query))
+						.FailOn(nameof(LanguagePilotRequest.Query)).FailWith(this._localizer["validation_required", nameof(LanguagePilotRequest.Query)]),
+					//dataset ids must not be empty
+					this.Spec()
+						.If(() => item.DatasetIds == null || !item.DatasetIds.Any())
+						.Must(() => false)
+						.FailOn(nameof(LanguagePilotRequest.DatasetIds)).FailWith(this._localizer["validation_required", nameof(LanguagePilotRequest.DatasetIds)]),
+				];
+			}
+		}
+	}
+
+	public class LanguagePilotResponse
+	{
+		public List<Metric> Metrics { get; set; }
+
+		public class Metric : BaseMetric
+		{
+			public List<TermFrequency> TermFrequencies { get; set; }
+
+			public class TermFrequency
+			{
+				public string Term { get; set; }
+				public decimal Frequency { get; set; }
+				public int Count { get; set; }
+			}
+
+			public MetricSentimentProfile SentimentProfile { get; set; }
+
+			public class MetricSentimentProfile
+			{
+				public string Label { get; set; }
+				public int PositiveTerms { get; set; }
+				public int NegativeTerms { get; set; }
+				public int NeutralTerms { get; set; }
+				public int TotalTerms { get; set; }
+				public decimal PolarityScore { get; set; }
+				public decimal SubjectivityScore { get; set; }
+			}
+
+			public List<Collocation> Collocations { get; set; }
+			public class Collocation
+			{
+				public List<string> Terms { get; set; }
+				public int Count { get; set; }
+				public decimal AssociationScore { get; set; }
+			}
+		}
+
+		public List<BaseMetric> UsedChunks { get; set; }
+		public class BaseMetric
+		{
+			public Guid DatasetId { get; set; }
+			public string ObjectId { get; set; }
+			public int Similarity { get; set; }
+		}
+	}
+}
